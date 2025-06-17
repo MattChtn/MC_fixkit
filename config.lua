4cec3700363f61228ce79b8c28ce5e5c9ad8a3e6
MattChtn <jbraultmattheo36500@gmail.com>
2025-06-17 14:55:04 +0200

    config.lua

--- config.lua
+++ config.lua
@@ -1,1 +1,4 @@
+Config = {}
 
+Config.Locale = "fr" -- Available languages : "fr"/"en" | Langues disponibles : "fr"/"en"
+Config.Time = 5 -- Time in seconds. Default = 5 | Temps en secondes. Par défault = 5
