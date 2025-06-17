8554945a8fdcdc97996c27ce3cfae3aa6be0f64c
MattChtn <jbraultmattheo36500@gmail.com>
2025-06-17 14:58:59 +0200

    target.lua

--- client/target.lua
+++ client/target.lua
@@ -1,1 +1,21 @@
+exports.ox_target:addGlobalVehicle({
+    {
+        name = 'fix_car',
+        icon = 'fa-solid fa-screwdriver-wrench',
+        label = locale("fixcar"),
+        items = "fixkit",
+        onSelect = function(data)
+            TriggerEvent('MC_fixkit:fixcar')
+            Wait(5000)
+            local vehicle = data.entity
+	        if DoesEntityExist(vehicle) then
+		        SetVehicleFixed(vehicle)
+		        SetVehicleDeformationFixed(vehicle)
+		        SetVehicleUndriveable(vehicle, false)
+		        SetVehicleEngineOn(vehicle, true, true)
+	        end
+        end
+    },
+})
 
+lib.locale(Config.Locale)
