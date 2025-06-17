7d1abff82a458b2fea744fd41cdd74d0502673d8
MattChtn <jbraultmattheo36500@gmail.com>
2025-06-17 14:55:27 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,1 +1,28 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
 
+author 'MattChtn'
+description 'Simple car fix kit'
+
+files {
+	'locales/*.json'
+}
+
+client_scripts {
+	'client/events.lua',
+	'client/target.lua',
+	'client/esx.lua',
+}
+
+shared_scripts {
+	'@ox_lib/init.lua',
+	'config.lua'
+}
+
+dependencies {
+	'ox_lib',
+	'ox_target',
+	'ox_inventory',
+	'es_extended',
+}
